edf70359243ac937d3573aee87b6de7d57a2efbc
Hyunjin Kim <69027218+jennn613@users.noreply.github.com>
2021-11-06 16:41:56 +0900

    finish
    Could you just check the rubric one more time and correct my grammar mistakes and re-write the names of variables if they aren't proper?

--- task1/task1/Program.cs
+++ task1/task1/Program.cs
@@ -19,20 +19,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-/*
- 
-that will support the counting and results of an electoral process where there are three candidates (‘April’, ‘Bill’, ‘Claire’). ✓
-Ask the users how many voters, would they have in their region, example 10. ✓
-Ask each voter to vote for one of the 3 candidates available and at the end of the number of voters, show the number of votes for each candidate and who won. 
-
-Expected code logic in your project:
--	Data-Entry: Where the total number of voters will be informed by the user ✓
--   Iteration: Where the user will inform the candidate (‘April’, ‘Bill’, ‘Claire’) until finish total voters. ✓
--	Decisions (if-then-else): Where the candidate will be identified, and the vote count will happen. ✓
--	Output of your application as described below. ✓
- 
- */
-
 namespace task1
 {
     class Program
@@ -41,67 +27,106 @@ namespace task1
         {
 
             //initialize variables
-            int voters = 0; // number of voters
+
+            int voters = 0; // the number of voters
             int voteNum = 0; // the number that a person voted
             int aprilVotedNum = 0; // show how many votes April get
             int billVotedNum = 0; // show how many votes Bill get
-            int claireVotedNum = 0; // show how many votes claire get         
+            int claireVotedNum = 0; // show how many votes claire get
+            bool keepGoing = true; 
+
+            /* ------------------------------------------------------------------ */
 
 
-            Console.Write("How many voters in your region? : ");
-            voters = Convert.ToInt32(Console.ReadLine());
+            //get the number of voters from user
+            do
+            {
+                try
+                {
+                    Console.Write("How many voters in your region?: ");
+                    voters = int.Parse(Console.ReadLine());
+                    Console.WriteLine(" ");
+                    keepGoing = false;
+                }
 
-            //Console.Write(voters);
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine("* You must enter a number. Please Enter one more time.");
+                    Console.WriteLine(" ");
+                    keepGoing = true;
+                } 
+
+            } while (keepGoing);
+
+
+            /* --------------------------------------------------------------------- */
+
+
+            //Ask users what candidate that they want to vote
+
             for (int i = 0; i < voters; i++)
             {
 
-                try
+                do
                 {
-                    Console.WriteLine("There are three candidates - enter number 1 for April, enter number 2 for Bill, enter number 3 for Claire");
-                    voteNum = Convert.ToInt32(Console.ReadLine());
-                    CalculateVoteNum(voteNum, aprilVotedNum, billVotedNum, claireVotedNum);
-                }
 
-                catch(FormatException fEx)
-                { 
-                    //If the user put a number not from 1 to 4
-                    Console.WriteLine("You must enter a number from 1 to 4 to vote for a candidater. Please Enter one more time.");
-                    voteNum = Convert.ToInt32(Console.ReadLine());
-                    CalculateVoteNum(voteNum, aprilVotedNum, billVotedNum, claireVotedNum);
-                }
+                    try
+                    {
+                        Console.Write("There are three candidates - enter number 1 for April, enter number 2 for Bill, enter number 3 for Claire: ");
+                        voteNum = int.Parse(Console.ReadLine());
+                        keepGoing = false;
+
+                        if (voteNum == 1)
+                        {
+                            aprilVotedNum++;
+                        }
+                        else if (voteNum == 2)
+                        {
+                            billVotedNum ++;
+
+                        }
+                        else if (voteNum == 3)
+                        {
+                            claireVotedNum++;
+
+                        }
+                        else
+                        {
+                            throw new FormatException("* You must enter an one digit number. Please enter one more time.");
+                        }
+                    }
+
+                    catch (FormatException fEx)
+                    {
+                        //If the user put a number that's not 1-4
+                        Console.WriteLine("* You must enter a number from 1 to 4 to vote for a candidater. Please enter one more time.");
+                        Console.WriteLine(" ");
+                        keepGoing = true;
+                    }
+
+                    catch (OverflowException oEx)
+                    {
+                        Console.WriteLine("* You must enter an one digit number. Please enter one more time.");
+                        Console.WriteLine(" ");
+                        keepGoing = true;
+                    }
+
+                } while (keepGoing);
+
             }
+
+
+            /* --------------------------------------------------------------------- */
 
             // Show people results
 
             Console.WriteLine("Candidate April: " + aprilVotedNum + " votes.");
             Console.WriteLine("Candidate Bill: " + billVotedNum + " votes.");
-            Console.WriteLine("Candidate Claire: " + claireVotedNum + "votes.");
+            Console.WriteLine("Candidate Claire: " + claireVotedNum + " votes.");
+
         }
 
-        static int CalculateVoteNum (int num, int april, int bill, int claire)
-        {
 
-            /* if (num == 1)
-            {
-                april++;
-                return april;
-            }
-            else if (num == 2)
-            {
-                bill++;
-                return bill;
- 
-            }
-            else if (num == 3)
-            {
-                claire++;
-                return claire;
-            }
-            else
-            {
-                //try catch
-            }
-             */
-        }
+          
     }
 }
