714019e11a602acbd5510bcb41ef69456cfc7427
Cloudiiiii <Yusufhafeji33@gmail.com>
2021-11-10 13:58:08 -0500

    done ^^
    >_< finally!

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -18,7 +18,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task2
+namespace YHHJKAssign3Task2S1
 {
     class Program
     {
@@ -31,22 +31,71 @@ namespace task2
             int populationB = 0;
             float growthA = 0;
             float growthB = 0;
+            bool key = true;
 
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter the size of population A");
+                    populationA = Convert.ToInt32(Console.ReadLine());
+                    key = false;
+                }
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.WriteLine("");
+                    key = true;
+                }
+            } while (key);
 
-            /*   -----------------------------------------------------    */
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter the size of population B");
+                    populationB = Convert.ToInt32(Console.ReadLine());
+                    key = false;
+                }
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.WriteLine("");
+                    key = true;
+                }
+            } while (key);
 
-            //get user inputs
-            /*Console.WriteLine("Enter the size of population A");
-            populationA = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter the rate of growth for population A as a percentage");
+                    growthA = float.Parse(Console.ReadLine()) / 100;
+                    key = false;
+                }
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.WriteLine("");
+                    key = true;
+                }
+            } while (key);
 
-            Console.WriteLine("Enter the size of population B (must be larger than population A)");
-            populationB = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the rate of growth for population A as a percentage");
-            growthA = float.Parse(Console.ReadLine()) / 100;
-
-            Console.WriteLine("Enter the rate of growth for population B as a percentage (must be less than growth rate of population A");
-            growthB = float.Parse(Console.ReadLine()) / 100;*/
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter the rate of growth for population B as a percentage");
+                    growthB = float.Parse(Console.ReadLine()) / 100;
+                    key = false;
+                }
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.WriteLine("");
+                    key = true;
+                }
+            } while (key);
 
             if (populationA < populationB && growthA > growthB)
             {
@@ -54,56 +103,10 @@ namespace task2
                 Console.WriteLine("Maintaining the growth rates, popullation A will be equal to or bigger than population B in " + years + " years."); // just to test if it works
 
             }
-            else if (populationA > populationB)
-            {
-                try
-                {
-                    Console.WriteLine("Enter the size of population A");
-                    populationA = Convert.ToInt32(Console.ReadLine());
-                }
-                catch (FormatException fEx)
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    Console.WriteLine("");
-                }
-                try
-                {
-                    Console.WriteLine("Enter the size of population B");
-                    populationB = Convert.ToInt32(Console.ReadLine());
-                }
-                catch (FormatException fEx)
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    Console.WriteLine("");
-                }
-            }
             else
-            { // when users entered A>B or growthA < growthB
-                try
-                {
-                    Console.WriteLine("Enter the rate of growth for population A as a percentage");
-                    growthA = float.Parse(Console.ReadLine()) / 100;
-                }
-                catch (FormatException fEx)
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    Console.WriteLine("");
-                }
-                try
-                {
-                    Console.WriteLine("Enter the rate of growth for population B as a percentage");
-                    growthB = float.Parse(Console.ReadLine()) / 100;
-                }
-                catch (FormatException fEx)
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    Console.WriteLine("");
-                }
-
+            {
+                throw new Exception("Population a must be smaller than population B, and growth rate of A must be larger than growth rate of B. Please restart the programa nd try again.");
             }
-            
-            Console.WriteLine("Maintaining the growth rates, popullation A will be equal to or bigger than population B in " + YearsForecast(populationA, populationB, growthA, growthB) + " years.");
-            
         }
 
         static int YearsForecast(int popA, int popB, float rateA, float rateB)
@@ -121,7 +124,7 @@ namespace task2
                 popA = Convert.ToInt32((year * increaseA) + popA);
                 popB = Convert.ToInt32((year * increaseB) + popB);
 
-                // I think we need to return the year to let users know how long does it take. And I can't get the meaning of maintaing the growth rates. I can't figure out what cna we do with them.
+                // I think we need to return the year to let users know how long does it take. And I can't get the meaning of maintaing the growth rates. I can't figure out what can we do with them.
             }
             return year;
         }
